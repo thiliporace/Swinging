f1a558acc9f616f23e71ae485401c3eda038b387
thiliporace <85422792+thiliporace@users.noreply.github.com>
2023-05-09 20:53:14 +0000

    Initial commit

---
+++ Swinging.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Swinging : MonoBehaviour
+{
+    [Header("Input")] 
+    public KeyCode swingKey = KeyCode.Mouse1;
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(swingKey)) StartSwing();
+        if(Input.GetKeyUp(swingKey)) StopSwing();
+    }
+
+    private void StartSwing()
+    {
+        
+    }
+
+    private void StopSwing()
+    {
+        
+    }
+}
